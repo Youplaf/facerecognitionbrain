a8f491c111822ced61e25c111cfd640384128adf
Youplaf <37189944+Youplaf@users.noreply.github.com>
2018-04-01 16:00:52 +0200

    some code changes

--- src/App.js
+++ src/App.js
@@ -105,7 +105,7 @@ class App extends Component {
         { route === 'home'
           ? <div>
               <Logo />
-              <Rank />
+              <Rank name={this.state.user.name} entries={this.state.user.entries} />
               <ImageLinkForm 
                 onInputChange={this.onInputChange} 
                 onButtonSubmit={this.onButtonSubmit} 
@@ -114,7 +114,7 @@ class App extends Component {
             </div>
           : (
               route === 'signin'
-              ? <Signin onRouteChange={this.onRouteChange} />
+              ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
               : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
             )
         }
